a975316f7bdcee405398eb878f80229e48a2b25f
Your Name <you@example.com>
2023-10-16 13:06:44 +0300

    added tests for clear,ensure capacity...

--- Collection.Tests/UnitTest1.cs
+++ Collection.Tests/UnitTest1.cs
@@ -55,7 +55,50 @@ namespace Collection.Tests
            
 
         }
+        [Test]
+        public void TestEnsureCapacity()
+        {
 
+            //Arrange
+           var nums = new Collection<int>(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+
+            //Act
+            nums.Add(17);
+
+            //Assert
+            Assert.That(nums.Capacity, Is.EqualTo(32));
+        }
+
+        [Test]
+        public void TestClear()
+        {
+
+            //Arrange
+            var nums = new Collection<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+
+            //Act
+            nums.Clear();
+
+            //Assert
+            Assert.That(nums.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        [Timeout(5000)] // used so it doesnt take forever
+
+        public void TestCollection1ThousandItems() {
+
+            const int itemsCount = 1000; // 
+            var nums = new Collection<int>();
+            nums.AddRange(Enumerable.Range(1, itemsCount).ToArray()); // adding all numbers from 1 to 1000 to the collection
+            Assert.That(nums.Count == itemsCount);
+            Assert.That(nums.Capacity >= nums.Count);
+            for (int i = itemsCount - 1; i >= 0; i--) {
+                nums.RemoveAt(i);
+            }
+            Assert.That(nums.ToString() == "[]");
+            Assert.That(nums.Capacity >= nums.Count);
+        }
     }
 
 
