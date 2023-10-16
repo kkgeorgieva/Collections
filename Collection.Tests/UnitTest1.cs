891178a8683eb65fd69a621b3b87413411b7d7e2
Your Name <you@example.com>
2023-10-16 13:44:38 +0300

    54% code coverage

--- Collection.Tests/UnitTest1.cs
+++ Collection.Tests/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace Collection.Tests
     public class CollectionTests
     {
 
+
         [Test]
         public void TestEmptyConstructor()
         {
@@ -17,7 +18,7 @@ namespace Collection.Tests
             //Assert
             Assert.AreEqual(0, nums.Count);
             Assert.AreEqual(16, nums.Capacity);
-            Assert.That(nums.ToString(), Is.EqualTo( "[]"));
+            Assert.That(nums.ToString(), Is.EqualTo("[]"));
         }
 
         [Test]
@@ -33,26 +34,28 @@ namespace Collection.Tests
         }
 
         [Test]
-        public void TestCollectionAdd() { 
-         
+        public void TestCollectionAdd()
+        {
+
             var nums = new Collection<int>(5);
             nums.Add(1);
-            Assert.That(nums.Count,Is.EqualTo(2));
-        
+            Assert.That(nums.Count, Is.EqualTo(2));
+
         }
 
         [Test]
-        public void TestCollectionAddRange() {
-        
+        public void TestCollectionAddRange()
+        {
+
             //Arrange
-            var nums = new Collection<int>(5,3,4);
-            int[] numsToAdd = new int[] {1,2,3};
+            var nums = new Collection<int>(5, 3, 4);
+            int[] numsToAdd = new int[] { 1, 2, 3 };
 
             //Act
             nums.AddRange(numsToAdd);
             //Assert
             Assert.That(nums.Count, Is.EqualTo(6));
-           
+
 
         }
         [Test]
@@ -60,7 +63,7 @@ namespace Collection.Tests
         {
 
             //Arrange
-           var nums = new Collection<int>(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+            var nums = new Collection<int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
 
             //Act
             nums.Add(17);
@@ -86,20 +89,41 @@ namespace Collection.Tests
         [Test]
         [Timeout(5000)] // used so it doesnt take forever
 
-        public void TestCollection1ThousandItems() {
+        public void TestCollection1ThousandItems()
+        {
 
             const int itemsCount = 1000; // 
             var nums = new Collection<int>();
             nums.AddRange(Enumerable.Range(1, itemsCount).ToArray()); // adding all numbers from 1 to 1000 to the collection
             Assert.That(nums.Count == itemsCount);
             Assert.That(nums.Capacity >= nums.Count);
-            for (int i = itemsCount - 1; i >= 0; i--) {
+            for (int i = itemsCount - 1; i >= 0; i--)
+            {
                 nums.RemoveAt(i);
             }
             Assert.That(nums.ToString() == "[]");
             Assert.That(nums.Capacity >= nums.Count);
         }
+
+
+        [TestCase("Peter", 0, "Peter")]
+        [TestCase("Peter, Maria , George", 0, "Peter")]
+        [TestCase("Peter, Maria, George", 1, "Maria")]
+        [TestCase("Peter, Maria, George", 2, "George")]
+
+        //Defining the values from the testcase in the method braces
+        public void TestCollectionGetByValidIndex(String data, int index, String expectedValue)
+        {
+            //Arrange
+            var nums = new Collection<String>(data.Split(", "));
+
+            //Act 
+            var actual = nums[index];
+
+            //Assert
+            Assert.That(actual, Is.EqualTo(expectedValue));
+
+        }
+
     }
-
-
 }
